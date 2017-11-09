dae026261c2f3fa3f0f763b5dad613a9c3facecc
Naszradi <naszradi@gmail.com>
2017-11-09 21:15:13 +0100

    setup project

---
+++ src/main/java/hu/elte/alkfejl/alkfejl_bead_2017/AlkfejlBead2017Application.java
@@ -0,0 +1,12 @@
+package hu.elte.alkfejl.alkfejl_bead_2017;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlkfejlBead2017Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlkfejlBead2017Application.class, args);
+	}
+}
---
+++ src/test/java/hu/elte/alkfejl/alkfejl_bead_2017/AlkfejlBead2017ApplicationTests.java
@@ -0,0 +1,16 @@
+package hu.elte.alkfejl.alkfejl_bead_2017;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AlkfejlBead2017ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
