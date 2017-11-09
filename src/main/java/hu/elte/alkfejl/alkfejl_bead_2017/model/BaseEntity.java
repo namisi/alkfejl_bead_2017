1d199d8d64df8a409555c3fe82d51b5161b51cf9
Naszradi <naszradi@gmail.com>
2017-11-09 21:21:09 +0100

    create BaseEntity

---
+++ src/main/java/hu/elte/alkfejl/alkfejl_bead_2017/model/BaseEntity.java
@@ -0,0 +1,17 @@
+package hu.elte.alkfejl.alkfejl_bead_2017.model;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Data
+@MappedSuperclass
+public class BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private String id;
+
+    @Version
+    private int version;
+}
