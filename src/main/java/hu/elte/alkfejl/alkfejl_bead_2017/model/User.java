d63d5bfa9a85c0176bf341302afb34b55ab466dd
Naszradi <naszradi@gmail.com>
2017-11-09 21:24:48 +0100

    create User

---
+++ src/main/java/hu/elte/alkfejl/alkfejl_bead_2017/model/User.java
@@ -0,0 +1,34 @@
+package hu.elte.alkfejl.alkfejl_bead_2017.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "USERS")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(callSuper = true)
+public class User extends BaseEntity {
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, unique = true)
+    private Role role;
+
+    private enum Role {
+        GUEST, USER, ADMIN
+    }
+}
